7ce04276404b4b5ab5d6031b453adeb7fbd16710
zhongrongguang <286371244@qq.com>
2018-04-28 23:12:34 +0800

    更新gradle文件和测试类的注释

--- app/src/main/java/cicinnus/com/aartest/MainActivity.java
+++ app/src/main/java/cicinnus/com/aartest/MainActivity.java
@@ -13,5 +13,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         AARUtil aarUtil = new AARUtil();
+        //使用本地aar会发现无法使用依赖传递,只能通过使用本地maven仓库,或者使用maven私服解决
+//        AMap
     }
 }
